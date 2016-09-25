93a1bb0a925de436d48d57f4289f419d27b02a3b
Alex Jordan <alex@strugee.net>
2016-09-25 02:03:22 -0700

    Write the module

---
+++ index.js
@@ -0,0 +1,31 @@
+/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+'use strict';
+
+var select = require('unist-util-select');
+
+function rewrite(ast, file, next) {
+	select(ast, 'heading').forEach(function(node) {
+		// TODO: should we refuse if the depth is 6?
+		node.depth -= 1;
+	});
+
+	next();
+};
+
+module.exports = function(remark, options) {
+	return rewrite;
+};
